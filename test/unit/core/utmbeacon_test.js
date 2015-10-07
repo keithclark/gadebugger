a054750e15b72262dac1f5be1d1b948680d83b8a
Keith Clark <keith@keithclark.co.uk>
2015-10-07 13:09:57 +0100

    Add missing tests for utmBeacon.js

--- test/unit/core/utmbeacon_test.js
+++ test/unit/core/utmbeacon_test.js
@@ -68,5 +68,56 @@ module.exports = {
             test.strictEqual(beacon.userTimings.label, 'Label');
             test.done();
         }
+    },
+    'Custom Vars': {
+        'using concurrent slots': function (test) {
+            var beacon = new UtmBeacon('http://www.google-analytics.com/__utm.gif?utmwv=5.6.7&utms=15&utmn=1494307825&utmhn=10.0.0.8&utme=8(Name%201*Name%202*Name%203*Name%204*Name%205)9(Value%201*Value%202*Value%203*Value%204*Value%205)11(1*2*4!1*2)&utmcs=UTF-8&utmsr=1440x900&utmvp=1438x474&utmsc=24-bit&utmul=en-us&utmje=1&utmfl=19.0%20r0&utmdt=Google%20Analytics%20Test%20Page&utmhid=2131029259&utmr=0&utmp=%2Fcustom-var-test&utmht=1444206336663&utmac=UA-AAAA-B&utmcc=__utma%3D196898604.1628777192.1444173499.1444173499.1444206337.2%3B%2B__utmz%3D196898604.1444173499.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B&utmjid=&utmu=6QCLAAAAAAAAAAAAAEAAQAAE~');
+            test.strictEqual(beacon.customVars.length, 5);
+            test.deepEqual(beacon.customVars[0], {slot: 1, key: 'Name 1', value: 'Value 1', scope: 1});
+            test.deepEqual(beacon.customVars[1], {slot: 2, key: 'Name 2', value: 'Value 2', scope: 2});
+            test.deepEqual(beacon.customVars[2], {slot: 3, key: 'Name 3', value: 'Value 3', scope: 3});
+            test.deepEqual(beacon.customVars[3], {slot: 4, key: 'Name 4', value: 'Value 4', scope: 1});
+            test.deepEqual(beacon.customVars[4], {slot: 5, key: 'Name 5', value: 'Value 5', scope: 2});
+            test.done();
+        },
+        'not using concurrent slots': function (test) {
+            var beacon = new UtmBeacon('http://www.google-analytics.com/__utm.gif?utmwv=5.6.7&utms=16&utmn=2096985664&utmhn=10.0.0.8&utme=8(Name%201*3!Name%203*5!Name%205)9(Value%201*3!Value%203*5!Value%205)11(1*5!2)&utmcs=UTF-8&utmsr=1440x900&utmvp=1438x474&utmsc=24-bit&utmul=en-us&utmje=1&utmfl=19.0%20r0&utmdt=Google%20Analytics%20Test%20Page&utmhid=2131029259&utmr=0&utmp=%2Fcustom-var-test-2&utmht=1444206336696&utmac=UA-AAAA-B&utmcc=__utma%3D196898604.1628777192.1444173499.1444173499.1444206337.2%3B%2B__utmz%3D196898604.1444173499.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B&utmjid=&utmu=6QCLAgAAAAAAAAAAAEAAQAAE~');
+            test.strictEqual(beacon.customVars.length, 3);
+            test.deepEqual(beacon.customVars[0], {slot: 1, key: 'Name 1', value: 'Value 1', scope: 1});
+            test.deepEqual(beacon.customVars[1], {slot: 3, key: 'Name 3', value: 'Value 3', scope: 3});
+            test.deepEqual(beacon.customVars[2], {slot: 5, key: 'Name 5', value: 'Value 5', scope: 2});
+            test.done();
+        }
+    },
+    'Social': function (test) {
+        var beacon = new UtmBeacon('http://www.google-analytics.com/__utm.gif?utmt=social&utmsn=network&utmsa=action&utmsid=target');
+        test.strictEqual(beacon.type, 'social');
+        test.strictEqual(beacon.social.network, 'network');
+        test.strictEqual(beacon.social.action, 'action');
+        test.strictEqual(beacon.social.target, 'target');
+        test.done();
+    },
+    'Transaction': {
+        'transaction': function (test) {
+            var beacon = new UtmBeacon('http://www.google-analytics.com/__utm.gif?utmwv=5.6.7&utms=11&utmn=747978421&utmhn=10.0.0.8&utmt=tran&utmtid=1234&utmtst=affiliation&utmtto=12.34&utmttx=8.90&utmtsp=5.67&utmtci=city&utmtrg=state&utmtco=country&utmcs=UTF-8&utmsr=1440x900&utmvp=1438x474&utmsc=24-bit&utmul=en-us&utmje=1&utmfl=19.0%20r0&utmdt=Google%20Analytics%20Test%20Page&utmhid=2131029259&utmr=0&utmp=%2F&utmht=1444206336623&utmac=UA-AAAA-B&utmcc=__utma%3D196898604.1628777192.1444173499.1444173499.1444206337.2%3B%2B__utmz%3D196898604.1444173499.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B&utmjid=&utmu=6ACLAAAAAAAAAAAAAEAAAAAE~');
+            test.strictEqual(beacon.type, 'transaction');
+            test.strictEqual(beacon.transaction.id, 1234);
+            test.strictEqual(beacon.transaction.affiliation, 'affiliation');
+            test.strictEqual(beacon.transaction.revenue, 12.34);
+            test.strictEqual(beacon.transaction.shipping, 5.67);
+            test.strictEqual(beacon.transaction.tax, 8.90);
+            test.done();
+        },
+        'item': function (test) {
+            var beacon = new UtmBeacon('http://www.google-analytics.com/__utm.gif?utmwv=5.6.7&utms=12&utmn=124848449&utmhn=10.0.0.8&utmt=item&utmtid=1234&utmipc=sku&utmipn=name&utmiva=category&utmipr=12.34&utmiqt=1&utmcs=UTF-8&utmsr=1440x900&utmvp=1438x474&utmsc=24-bit&utmul=en-us&utmje=1&utmfl=19.0%20r0&utmdt=Google%20Analytics%20Test%20Page&utmhid=2131029259&utmr=0&utmp=%2F&utmht=1444206336624&utmac=UA-AAAA-B&utmcc=__utma%3D196898604.1628777192.1444173499.1444173499.1444206337.2%3B%2B__utmz%3D196898604.1444173499.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B&utmjid=&utmu=6ACLAAAAAAAAAAAAAEAAAAAE~');
+            test.strictEqual(beacon.type, 'item');
+            test.strictEqual(beacon.transactionItem.transactionId, 1234);
+            test.strictEqual(beacon.transactionItem.name, 'name');
+            test.strictEqual(beacon.transactionItem.sku, 'sku');
+            test.strictEqual(beacon.transactionItem.category, 'category');
+            test.strictEqual(beacon.transactionItem.price, 12.34);
+            test.strictEqual(beacon.transactionItem.quantity, 1);
+            test.done();
+        }
     }
 }
